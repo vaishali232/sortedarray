730b207715e52b382394d200f381b39acd304fbe
vaishali232 <51689354+vaishali232@users.noreply.github.com>
2019-06-11 19:51:38 +0530

    Create sort.c

---
+++ sort.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main()
+{
+    
+    int n,t;
+    scanf("%d",&n);
+    int a[n];
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d ",&a[i]);
+    }
+    for(int i=0;i<n;i++)
+    {
+        for(int j=i+1;j<n;j++)
+        {
+            if(a[i]<a[j])
+            {
+                t=a[i];
+                a[i]=a[j];
+                a[j]=t;
+            }
+        }
+    }
+    for(int i=0;i<n;i++)
+    {
+    printf("%d ",a[i]);
+    }
+    
+}
